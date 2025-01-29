f2c9ec61f7fea24db201d2ce9bca2c392f466865
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-01-29 21:04:55 +0000

    tests configuration

--- tests/inc/minishell_tests.h
+++ tests/inc/minishell_tests.h
@@ -3,15 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   minishell_tests.h                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 13:57:40 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/01/28 13:58:10 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/01/29 20:17:51 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_TESTS_H
 # define MINISHELL_TESTS_H
+# include "mock.h"
 # include "minishell.h"
+
+int	program_main(void);
 
 #endif
--- tests/src/main.c
+++ tests/src/main.c
@@ -3,16 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: jarao-de <jarao-de@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 13:26:10 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/01/28 14:24:09 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/01/29 20:09:16 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	program_main(void);
+#include "minishell_tests.h"
 
 int	main(void)
 {
-	return(program_main());
+	t_readline_control readline_control;
+
+	(void) readline_control;
+	set_readline_predefined_response("hello");
+	activate_readline_failure_mode(2);
+	readline_control = get_readline_control_status();
+	program_main();
+	readline_control = get_readline_control_status();
+	return (0);
 }

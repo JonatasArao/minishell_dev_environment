7c4133d284ad3c6a0cf644ff0517abe68eecc200
Guilherme Gomes de Oliveira <45400708+GuilhermeGors@users.noreply.github.com>
2025-01-30 12:57:22 -0300

    filter modularization refined

--- prototype/command_filter-gugomes-.c
+++ prototype/command_filter-gugomes-.c
@@ -6,7 +6,7 @@
 /*   By: gugomes- <gugomes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 11:00:12 by gugomes-          #+#    #+#             */
-/*   Updated: 2025/01/30 12:25:09 by gugomes-         ###   ########.fr       */
+/*   Updated: 2025/01/30 12:53:19 by gugomes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <ctype.h>
-
-int is_special_char(char c)
-{
- 	return (c == '<' || c == '>' || c == '|');
-}
 
 typedef struct s_token
 {
@@ -71,16 +66,48 @@ void free_tokens(t_token *tokens)
 		free(temp);
 	}
 }
+void handle_quotes(char **start, t_token **tokens)
+{
+	char *end = *start + 1;
+	while (*end && *end != '"')
+		end++;
+	if (*end == '"')
+		end++;
+	char *token_value = ft_substr(*start, 0, end - *start);
+	add_token(tokens, token_value);
+	*start = end;
+}
+
+void handle_metachar(char **start, t_token **tokens)
+{
+	char *token_value;
+
+	if (!ft_strncmp("<<", *start, 2) || !ft_strncmp(">>", *start, 2))
+	{
+		token_value = ft_substr(*start, 0, 2);
+		*start += 2;
+	}
+	else
+	{
+		token_value = ft_substr(*start, 0, 1);
+		*start += 1;
+	}
+	add_token(tokens, token_value);
+}
+
+void handle_regular_chars(char **start, t_token **tokens)
+{
+	char *end = *start;
+	while (*end && !isspace(*end) && !ft_strchr(">|<", *end))
+		end++;
+	char *token_value = ft_substr(*start, 0, end - *start);
+	add_token(tokens, token_value);
+	*start = end;
+}
 
 void tokenize(char *input, t_token **tokens)
 {
-	char    *start;
-	char    *end;
-	char    *token_value;
-	int     in_quotes;
-
-	start = input;
-	in_quotes = 0;
+	char *start = input;
 	while (*start)
 	{
 		while (*start && isspace(*start))
@@ -88,34 +115,11 @@ void tokenize(char *input, t_token **tokens)
 		if (*start == '\0')
 			break;
 		if (*start == '"')
-		{
-			end = start + 1;
-			while (*end && *end != '"')
-				end++;
-			if (*end == '"')
-				end++;
-			token_value = ft_substr(start, 0, end - start);
-			add_token(tokens, token_value);
-			start = end;
-		}
-		else if (is_special_char(*start))
-		{
-			end = start + 1;
-			if ((*start == '<' && *end == '<') || (*start == '>' && *end == '>'))
-				end++;
-			token_value = ft_substr(start, 0, end - start);
-			add_token(tokens, token_value);
-			start = end;
-		}
+			handle_quotes(&start, tokens);
+		else if (ft_strchr(">|<", *start))
+			handle_metachar(&start, tokens);
 		else
-		{
-			end = start;
-			while (*end && !isspace(*end) && !is_special_char(*end))
-				end++;
-			token_value = ft_substr(start, 0, end - start);
-			add_token(tokens, token_value);
-			start = end;
-		}
+			handle_regular_chars(&start, tokens);
 	}
 }
 

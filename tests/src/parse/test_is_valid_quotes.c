f19817f00ce5509330d7cef8c9eec37307884b11
Jonatas Arão de Carvalho Neves <51247074+JonatasArao@users.noreply.github.com>
2025-02-05 18:08:45 -0300

    is_valid_quotes fixed

--- tests/src/parse/test_is_valid_quotes.c
+++ tests/src/parse/test_is_valid_quotes.c
@@ -6,7 +6,7 @@
 /*   By: jarao-de <jarao-de@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 16:53:45 by jarao-de          #+#    #+#             */
-/*   Updated: 2025/02/05 17:15:12 by jarao-de         ###   ########.fr       */
+/*   Updated: 2025/02/05 18:07:11 by jarao-de         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -220,6 +220,102 @@ MU_TEST(test_is_valid_quotes_long_input)
 	mu_assert_int_eq(expected_result, actual_result);
 }
 
+MU_TEST(test_is_valid_quotes_closed_and_unclosed_single_quote_input)
+{
+	// ARRANGE
+	const char *input;
+	int expected_result;
+	int actual_result;
+
+	// ACT
+	input = "'echo' 'Hello";
+	expected_result = 0;
+	actual_result = is_valid_quotes(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_is_valid_quotes_closed_and_unclosed_souble_quote_input)
+{
+	// ARRANGE
+	const char *input;
+	int expected_result;
+	int actual_result;
+
+	// ACT
+	input = "\"echo\" \"Hello";
+	expected_result = 0;
+	actual_result = is_valid_quotes(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_is_valid_quotes_unclosed_double_quote_input_with_single_quote)
+{
+	// ARRANGE
+	const char *input;
+	int expected_result;
+	int actual_result;
+
+	// ACT
+	input = "\"echo Hello'";
+	expected_result = 0;
+	actual_result = is_valid_quotes(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_is_valid_quotes_unclosed_single_quote_input_with_double_quote)
+{
+	// ARRANGE
+	const char *input;
+	int expected_result;
+	int actual_result;
+
+	// ACT
+	input = "'echo Hello\"";
+	expected_result = 0;
+	actual_result = is_valid_quotes(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_is_valid_quotes_multiple_unclosed_quotes_input)
+{
+	// ARRANGE
+	const char *input;
+	int expected_result;
+	int actual_result;
+
+	// ACT
+	input = "'echo Hello\" '";
+	expected_result = 1;
+	actual_result = is_valid_quotes(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_is_valid_quotes_multiple_closed_quotes_input)
+{
+	// ARRANGE
+	const char *input;
+	int expected_result;
+	int actual_result;
+
+	// ACT
+	input = "'echo' \"Hello\"";
+	expected_result = 1;
+	actual_result = is_valid_quotes(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
 MU_TEST_SUITE(is_valid_quotes_test_suite)
 {
 	MU_RUN_TEST(test_is_valid_quotes_no_quotes_input);
@@ -235,6 +331,12 @@ MU_TEST_SUITE(is_valid_quotes_test_suite)
 	MU_RUN_TEST(test_is_valid_quotes_special_characters_input);
 	MU_RUN_TEST(test_is_valid_quotes_escape_sequence_input);
 	MU_RUN_TEST(test_is_valid_quotes_long_input);
+	MU_RUN_TEST(test_is_valid_quotes_closed_and_unclosed_single_quote_input);
+	MU_RUN_TEST(test_is_valid_quotes_closed_and_unclosed_souble_quote_input);
+	MU_RUN_TEST(test_is_valid_quotes_unclosed_double_quote_input_with_single_quote);
+	MU_RUN_TEST(test_is_valid_quotes_unclosed_single_quote_input_with_double_quote);
+	MU_RUN_TEST(test_is_valid_quotes_multiple_unclosed_quotes_input);
+	MU_RUN_TEST(test_is_valid_quotes_multiple_closed_quotes_input);
 }
 
 int test_is_valid_quotes(void)
